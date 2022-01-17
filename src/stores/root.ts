0be4ca5fd244c7d99d1293be981d48826615297d
tnorthern <traynorthern@yahoo.com>
2022-01-17 12:49:35 -0600

    Add observable

--- src/stores/root.ts
+++ src/stores/root.ts
@@ -1,15 +1,23 @@
 import { defineStore } from 'pinia'
+import { useProjects } from '~/composables/useProjects'
 
 interface RootState {
   appLoading: boolean
+  projects: any
 }
 
 export const useRootStore = defineStore({
   id: 'root',
   state: (): RootState => ({
     appLoading: false,
+    projects: [],
   }),
   actions: {
+    async initialize() {
+      const { fetch } = useProjects()
+      const { data } = await fetch()
+      this.projects = data
+    },
     toggleAppLoading(value: boolean) {
       if (typeof value !== 'undefined') this.appLoading = value
       else this.appLoading = !this.appLoading
