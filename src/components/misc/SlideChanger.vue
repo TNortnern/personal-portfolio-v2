106be52c0a851457b6487e70db92ecb1c9ee8377
tnorthern <traynorthern@yahoo.com>
2022-01-24 11:16:56 -0600

    Update controls on smaller screens

--- src/components/misc/SlideChanger.vue
+++ src/components/misc/SlideChanger.vue
@@ -1,7 +1,7 @@
 <template>
   <button class="group">
-    <SliderArrow class="text-app-light-blue w-16 h-16 block mx-auto duration-120 mb-2 ease-in transform group-hover:(opacity-83 scale-112)" :class="{ 'rotate-180': rotate }" />
-    <span v-show="includeText" class="block text-center group-hover:(text-app-light-blue duration-120 ease-in)">{{ rotate ? 'Next' : 'Previous' }}</span>
+    <SliderArrow class="text-app-light-blue w-13 h-13 lg:(w-15 h-15) block mx-auto duration-120 mb-2 ease-in transform group-hover:(opacity-83 scale-112)" :class="{ 'rotate-180': rotate }" />
+    <span v-show="includeText" class="block text-center text-xs lg:text-base group-hover:(text-app-light-blue duration-120 ease-in)">{{ rotate ? 'Next' : 'Previous' }}</span>
   </button>
 </template>
 
