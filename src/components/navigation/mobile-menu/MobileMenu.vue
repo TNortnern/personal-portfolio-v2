abc946283b64a19e69e21522a7a3b7de223ff4fe
tnorthern <traynorthern@yahoo.com>
2022-01-24 16:23:16 -0600

    Mobile menu

--- src/components/navigation/mobile-menu/MobileMenu.vue
+++ src/components/navigation/mobile-menu/MobileMenu.vue
@@ -1,9 +1,24 @@
 <template>
-  <div :class="isOpen() ? 'opacity-100 scale-100 translate-x-0' : 'opacity-0 scale-0 -translate-y-100'" class="duration-400 lg:hidden z-25 transform ease-in-out bg-app-dark-blue h-screen text-white inset-0 fixed" />
+  <div :class="isOpen() ? 'opacity-100 scale-100 translate-x-0' : 'opacity-0 scale-0 -translate-y-100'" class="duration-400 lg:hidden z-25 transform ease-in-out bg-app-dark-blue h-screen text-white inset-0 fixed flex items-center justify-center">
+    <div class="text-5xl flex flex-col space-y-14 relative bottom-8">
+      <a
+        v-for="route in allRoutes" :key="route.text" :class="activeHash === route.href ? 'text-app-light-blue' : 'group-hover:text-app-light-blue'"
+        class="block" :href="route.href"
+      >
+        {{ route.text }}
+      </a>
+    </div>
+  </div>
   <Hamburger />
 </template>
 
 <script lang="ts" setup>
+import { useActiveRoute } from '~/composables/useActiveRoute'
 import { useMobileMenu } from '~/composables/useMobileMenu'
+import { useRouteStore } from '~/stores/routes'
 const { isOpen } = useMobileMenu()
+const routeStore = useRouteStore()
+const allRoutes = routeStore.routes
+const { getActiveRoute } = useActiveRoute()
+const activeHash = computed(() => `#${getActiveRoute()}`)
 </script>
--- src/composables/useMobileMenu.ts
+++ src/composables/useMobileMenu.ts
@@ -1,6 +1,10 @@
 const expanded = ref(false)
 
 export function useMobileMenu() {
+  watch(expanded, (value) => {
+    if (value) document.querySelector('body').classList.add('overflow-y-hidden')
+    else document.querySelector('body').classList.remove('overflow-y-hidden')
+  })
   return {
     isOpen: () => expanded.value,
     toggle: () => expanded.value = !expanded.value,
