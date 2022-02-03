6db82c33df7aef756dde371f8efad73adfa7dd92
tnorthern <traynorthern@yahoo.com>
2022-02-03 09:15:00 -0600

    Remove default filter, defaulting to keep-alive

--- src/components/portfolio/projects/Projects.vue
+++ src/components/portfolio/projects/Projects.vue
@@ -10,52 +10,56 @@
     caption="Portfolio"
     title="My Works"
   >
-    <div class="flex flex-wrap justify-center text-lg gap-6 mt-16">
-      <!-- Temporarily disabling all because I'm still thinking about the design and I don't want a million projects showing on the initial load -->
-      <button
-        v-for="tab in tabs.filter((t) => t !== 'all')"
-        :key="tab"
-        class="rounded-full py-2.5 w-30 duration-150 capitalize"
-        :data-filter="tab === 'all' ? 'all' : '.' + tab"
-        :class="projectStore.getActiveCategoryTab === tab ? 'bg-app-light-blue text-white' : 'text-gray-400 hover:(bg-app-light-blue text-white bg-opacity-75)'"
-        @click="setActiveCategoryTab(tab)"
-      >
-        {{ `${tab}${tab !== 'all' ? 's' : ''}` }}
-      </button>
-    </div>
-    <div class="mix-grid mt-8">
-      <component
-        :is="project.category.title.toLowerCase() === 'website' ? 'router-link' : 'a'"
-        v-bind="{
-          ...project.category.title.toLowerCase() === 'website' && { to: `/projects/${project.title}` },
-          ...project.category.title.toLowerCase() !== 'website' && { href: project.hosted_url, target: '__blank' }
-        }"
-        v-for="project of projectStore.all" :key="project.id"
-        class="mix group bg-white shadow-2xl relative rounded-md overflow-hidden relative project-width"
-        :class="[project.category.title.toLowerCase(), {'h-[18.5rem]': ['email', 'package'].includes(project.category.title.toLowerCase()) }]"
-      >
-        <img v-if="!loadedItems.includes(project.id)" :data-item-id="project.id" class="lozad loading-image overflow-hidden h-full w-full" src="/imgload.svg" alt="loading image">
-        <picture v-else>
-          <source media="(min-width:769px)" :srcset="project?.media?.[0]?.formats?.large?.url">
-          <source media="(max-width:768px)" :srcset="project?.media?.[0]?.formats?.medium?.url">
-          <source media="(max-width:480px)" :srcset="project?.media?.[0]?.formats?.small?.url">
-          <img
-            :src="project?.media?.[0]?.url" class="overflow-hidden h-full  w-full"
-            :alt="project.title"
-          >
-        </picture>
-        <div class="absolute h-full w-full inset-0 bg-black bg-opacity-60 transform -translate-x-[100%] opacity-0 group-hover:(translate-x-0 opacity-100) duration-400" />
-        <div class="absolute h-full w-full inset-0 bg-black bg-opacity-60 transform translate-x-[100%] opacity-0 group-hover:(translate-x-0 opacity-100) duration-400" />
-        <div
-          class=" text-white absolute inset-0 h-full w-full flex justify-center items-center duration-200 opacity-0 group-hover:(opacity-100)"
+    <keep-alive>
+      <div class="flex flex-wrap justify-center text-lg gap-6 mt-16">
+        <!-- Temporarily disabling all because I'm still thinking about the design and I don't want a million projects showing on the initial load -->
+        <button
+          v-for="tab in tabs.filter((t) => t !== 'all')"
+          :key="tab"
+          class="rounded-full py-2.5 w-30 duration-150 capitalize"
+          :data-filter="tab === 'all' ? 'all' : '.' + tab"
+          :class="projectStore.getActiveCategoryTab === tab ? 'bg-app-light-blue text-white' : 'text-gray-400 hover:(bg-app-light-blue text-white bg-opacity-75)'"
+          @click="setActiveCategoryTab(tab)"
         >
-          <h1 class="text-4xl font-bold group-hover:(animate-fade-in-up animate-faster)">
-            {{ project.title }}
-          </h1>
-        </div>
-      </component>
-      <div class="project-width" />
-    </div>
+          {{ `${tab}${tab !== 'all' ? 's' : ''}` }}
+        </button>
+      </div>
+    </keep-alive>
+    <keep-alive>
+      <div class="mix-grid mt-8">
+        <component
+          :is="project.category.title.toLowerCase() === 'website' ? 'router-link' : 'a'"
+          v-bind="{
+            ...project.category.title.toLowerCase() === 'website' && { to: `/projects/${project.title}` },
+            ...project.category.title.toLowerCase() !== 'website' && { href: project.hosted_url, target: '__blank' }
+          }"
+          v-for="project of projectStore.all" :key="project.id"
+          class="mix group bg-white shadow-2xl relative rounded-md overflow-hidden relative project-width"
+          :class="[project.category.title.toLowerCase(), {'h-[18.5rem]': ['email', 'package'].includes(project.category.title.toLowerCase()) }]"
+        >
+          <img v-if="!loadedItems.includes(project.id)" :data-item-id="project.id" class="lozad loading-image overflow-hidden h-full w-full" src="/imgload.svg" alt="loading image">
+          <picture v-else>
+            <source media="(min-width:769px)" :srcset="project?.media?.[0]?.formats?.large?.url">
+            <source media="(max-width:768px)" :srcset="project?.media?.[0]?.formats?.medium?.url">
+            <source media="(max-width:480px)" :srcset="project?.media?.[0]?.formats?.small?.url">
+            <img
+              :src="project?.media?.[0]?.url" class="overflow-hidden h-full  w-full"
+              :alt="project.title"
+            >
+          </picture>
+          <div class="absolute h-full w-full inset-0 bg-black bg-opacity-60 transform -translate-x-[100%] opacity-0 group-hover:(translate-x-0 opacity-100) duration-400" />
+          <div class="absolute h-full w-full inset-0 bg-black bg-opacity-60 transform translate-x-[100%] opacity-0 group-hover:(translate-x-0 opacity-100) duration-400" />
+          <div
+            class=" text-white absolute inset-0 h-full w-full flex justify-center items-center duration-200 opacity-0 group-hover:(opacity-100)"
+          >
+            <h1 class="text-4xl font-bold group-hover:(animate-fade-in-up animate-faster)">
+              {{ project.title }}
+            </h1>
+          </div>
+        </component>
+        <div class="project-width" />
+      </div>
+    </keep-alive>
   </SectionBlock>
 </template>
 <script lang="ts" setup>
@@ -76,17 +80,17 @@ const tabs: Tab[] = [
 ]
 // weird type but it surpresses the ts warning..
 const loadedItems = ref<string[] | any[] | null[]>([])
-const initialize = useMixItUp('.mix-grid', {
+useMixItUp('.mix-grid', {
   animation: {
     animateResizeTargets: true,
   },
 })
-watch(() => initialize, (val: any) => {
-  // initalize mixer default value
-  val?.mixer.value.filter(projectStore.getActiveCategoryTab === 'all' ? 'all' : `.${projectStore.getActiveCategoryTab}`)
-}, {
-  deep: true,
-})
+// watch(() => initialize, (val: any) => {
+//   // initalize mixer default value
+//   val?.mixer.value.filter(projectStore.getActiveCategoryTab === 'all' ? 'all' : `.${projectStore.getActiveCategoryTab}`)
+// }, {
+//   deep: true,
+// })
 onMounted(async() => {
   const observer = lozad('.lozad', {
     rootMargin: '400px',
