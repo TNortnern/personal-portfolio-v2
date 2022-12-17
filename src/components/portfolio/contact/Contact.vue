4e1c747b84d1b06336b07247f9d25f6b58bbd9db
Tray Northern <traynorthern@yahoo.com>
2022-12-16 19:58:27 -0600

    update base

--- src/components/portfolio/contact/Contact.vue
+++ src/components/portfolio/contact/Contact.vue
@@ -143,7 +143,7 @@ const sendEmail = async(form: ContactForm) => {
     genericForm.value.initializeForm()
   }, 600)
   try {
-    await axios.post('https://personalport-be.herokuapp.com/email', {
+    await axios.post('https://personal-portfolio-be.onrender.com/email', {
       subject: `Message from website by: ${form.name}`,
       from: 'foodeater563@outlook.com',
       to: emailBase,
