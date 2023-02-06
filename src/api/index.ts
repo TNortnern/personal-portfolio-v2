12c0618ee69a2c3e2f18920501ea94847c14f3e4
Tray Northern <traynorthern@yahoo.com>
2023-02-06 00:42:53 -0600

    force

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 const api = axios.create({
-  baseURL: import.meta.env.VITE_DEV_MODE ? 'http://localhost:1331' : 'https://personal-portfolio-be.onrender.com',
+  baseURL: 'https://personal-portfolio-be.onrender.com',
 })
 export default api
